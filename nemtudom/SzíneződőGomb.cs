c500734d91465c7f31f29b053521170a00579e52
Tejecskekeke <elod.tempfli@gmail.com>
2024-02-29 13:04:39 +0100

    mamaaa

---
+++ nemtudom/SzíneződőGomb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nemtudom
+{
+    internal class SzíneződőGomb : Button
+    {
+        public SzíneződőGomb() 
+        { 
+
+            Height = 20;
+            Width = 20;
+            MouseClick += SzíneződőGomb_MouseClick;
+        
+        
+        }
+
+        private void SzíneződőGomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            BackColor = Color.Fuchsia;
+        }
+    }
+}
